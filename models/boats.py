2771323416236d30813aa66de5c0f4a2f43f7b8b
Mauro Sbicego <mauro@sbicego.ch>
2022-11-21 12:47:49 +0100

    Changed Boat Model

--- models/boats.py
+++ models/boats.py
@@ -7,7 +7,7 @@ from config import config
 class CreateBoat(BaseModel):
     name: str = Field(None, max_length=100)
     category: Literal[tuple(config["boatCategories"])]
-    total: int = Field(None, lt=100)
+    total: int = Field(None, lt=100, gt=0)
     cox: bool
 
 
