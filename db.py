e8dec89d6f0875a6623d1efbc15e8d18b5f0c281
Mauro Sbicego <mauro@sbicego.ch>
2022-11-25 19:17:54 +0100

    Changed Dockerfile

--- db.py
+++ db.py
@@ -3,10 +3,11 @@ import motor.motor_asyncio
 from fastapi_users.db import BeanieUserDatabase
 from dotenv import load_dotenv
 from models.users import User
-
+from logger import logger
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+logger.debug(DATABASE_URL)
 client = motor.motor_asyncio.AsyncIOMotorClient(
     DATABASE_URL, uuidRepresentation="standard"
 )
